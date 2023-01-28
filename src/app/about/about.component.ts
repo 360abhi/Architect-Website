6fe1e2abc0feaf5c78de96a0432e40d1b08a465c
Abhishek Chhawari <abhishekch360@gmail.com>
2023-01-28 17:51:38 +0530

    event binding

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,8 +12,21 @@ export class AboutComponent {
 
   aboutobject;
 
+  isSelected : boolean = false
+  name:string = ""
+  position:string = "" 
+
   constructor(aboutservice: Aboutservice){
     this.aboutobject = aboutservice.getAbouts();
+  }
+
+  contacted(name : any, position:any,event: Event){
+    this.isSelected = true;
+    // this.name  = name.ta
+    // this.position = "" 
+    this.name = name.innerText;
+    this.position = position.innerText;
+
   }
 
 }
